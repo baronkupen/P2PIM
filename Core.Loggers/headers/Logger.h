f793f8f1cc17c311eff29211416bffafcd2e5cef
baron <aaronkupen@zoominternet.net>
2014-11-11 20:35:46 -0500

    Cleaned up logger header.

--- Core.Loggers/headers/Logger.h
+++ Core.Loggers/headers/Logger.h
@@ -18,20 +18,20 @@
 
 namespace Core_Loggers {
 	class Logger : public Interfaces::ILogger, public Common::Interfaces::IDisposable {
-	private:
-		const Core_TypeWrappers::Interfaces::IOutStream &outStream;
-		const Core_DateTime::Interfaces::IDate &date;
-		const Core_DateTime::Interfaces::IDateTimeFormatter &dateTimeFormatter;
-		bool active;
-		bool disposed;
-	protected:
-		void dispose(const bool &disposing);
-	public:
-		void log(const std::string &message, const LogLevel &logLevel) const override;
-		void dispose() override;
-		Logger(const Core_TypeWrappers::Interfaces::IOutStream &outStream, const Core_DateTime::Interfaces::IDate &date, const Core_DateTime::Interfaces::IDateTimeFormatter &dateTimeFormatter);
-		Logger(const Core_TypeWrappers::Interfaces::IOutStream &outStream, const Core_DateTime::Interfaces::IDate &date, const Core_DateTime::Interfaces::IDateTimeFormatter &dateTimeFormatter, bool active);
-		~Logger();
+		private:
+			const Core_TypeWrappers::Interfaces::IOutStream &outStream;
+			const Core_DateTime::Interfaces::IDate &date;
+			const Core_DateTime::Interfaces::IDateTimeFormatter &dateTimeFormatter;
+			bool active;
+			bool disposed;
+		protected:
+			void dispose(const bool &disposing);
+		public:
+			void log(const std::string &message, const LogLevel &logLevel) const override;
+			void dispose() override;
+			Logger(const Core_TypeWrappers::Interfaces::IOutStream &outStream, const Core_DateTime::Interfaces::IDate &date, const Core_DateTime::Interfaces::IDateTimeFormatter &dateTimeFormatter);
+			Logger(const Core_TypeWrappers::Interfaces::IOutStream &outStream, const Core_DateTime::Interfaces::IDate &date, const Core_DateTime::Interfaces::IDateTimeFormatter &dateTimeFormatter, bool active);
+			virtual ~Logger();
 	};
 }
 
